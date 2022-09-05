6b09f71fd4710a62563e8fa0c1fff87c1cb2528c
Iryna Hrynda <hryndairyna1@gmail.com>
2022-09-05 10:33:41 +0300

    implemented unit tests

--- src/test/java/com/example/todolistapp/security/CustomUserDetailsServiceTest.java
+++ src/test/java/com/example/todolistapp/security/CustomUserDetailsServiceTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
